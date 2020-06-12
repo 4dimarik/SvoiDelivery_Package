2251a1b115ca37fde34e95a2cdd90a71880aa0ee
4dimarik <4dimariK@gmail.com>
2020-06-12 13:27:33 +0300

    add setMessageData

--- src/Request.php
+++ src/Request.php
@@ -9,7 +9,16 @@ use TelegramBot\Api\Types\Update;
 
 class Request
 {
+    const MESSAGE_FIELDS = ["cId", "mId", "text"];
+    const USER_FIELDS = ["uid", "first_name", "contact"];
     private $Update;
+
+    /**
+     * @var array
+     */
+    private $data = [
+        "message"=>[]
+    ];
 
     /**
      * Request constructor.
@@ -32,5 +41,48 @@ class Request
             return false;
         }
 
+    }
+    public function gettData(string $type): void
+    {
+        switch ($type){
+            case'message':
+                $this->setMessageData();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private function setMessageData(): void
+    {
+        $this
+            ->addPropertyIsNull("cId", $this->Update->getMessage()->getChat()->getId())
+            ->addPropertyIsNull("mId", $this->Update->getMessage()->getMessageId())
+            ->addPropertyIsNull("text", $this->Update->getMessage()->getText())
+            ->addPropertyIsNull("date", $this->Update->getMessage()->getDate())
+            ->addUserContact($this->Update->getMessage()->getContact());
+    }
+
+    /**
+     * @return array
+     */
+    public function getData(): array
+    {
+        return $this->data;
+    }
+
+    private function addPropertyIsNull(string $name, string $value): Request
+    {
+        if (!is_null($value)){
+            $this->data[$name] = $value;
+        }
+        return $this;
+    }
+    private function addUserContact(string $contact): Request
+    {
+        if (!is_null($contact)){
+            $this->data['contact'] = str_replace('+7', '8', $contact);
+        }
+        return $this;
     }
 }
