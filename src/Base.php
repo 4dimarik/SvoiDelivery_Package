31b311fd5c4d0f8a3fe8d23db6ab8d67760b70e3
4dimarik <4dimariK@gmail.com>
2020-06-12 16:56:59 +0300

    add DB

---
+++ src/Base.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace wooShopTBot;
+
+
+class Base
+{
+    const CART_TBL = 'cart';
+    const USER_TBL = 'users';
+    const ORDER_TBL = 'orders';
+    const ERR_TBL = "errors";
+    const REQUEST_TBL = "requests";
+}
---
+++ src/DB.php
@@ -0,0 +1,62 @@
+<?php
+
+
+namespace wooShopTBot;
+
+
+use stdClass as stdClassAlias;
+
+class DB extends SafeMySQL
+{
+    protected $query;
+
+    protected function reset(): void
+    {
+        $this->query = new stdClassAlias;
+    }
+
+    /**
+     * Построение базового запроса SELECT.
+     * @param string $table
+     * @param array $fields
+     * @return DB
+     */
+    public function select(string $table, array $fields): DB
+    {
+        $this->reset();
+        $this->query->base = "SELECT " . ($fields!==[])?implode(", ", $fields):'*' . " FROM " . $table;
+        $this->query->type = 'select';
+
+        return $this;
+    }
+
+    /**
+     * Добавление условия WHERE.
+     * @param string $field
+     * @param string $value
+     * @param string $operator
+     * @return DB
+     */
+    public function where(string $field, string $value, string $operator = '='): DB
+    {
+        if (in_array($this->query->type, ['select', 'update', 'delete'])) {
+            $this->query->where[] = "$field $operator '$value'";
+        }
+
+        return $this;
+    }
+    /**
+     * Получение окончательной строки запроса.
+     */
+    public function getSafeMySQL(): string
+    {
+        $query = $this->query;
+        $sql = $query->base;
+        if (!empty($query->where)) {
+            $sql .= " WHERE " . implode(' AND ', $query->where);
+        }
+        return $sql;
+    }
+
+
+}
