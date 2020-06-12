d3c809e1fb36fe4c79e827bb62efd8bf19e5934e
4dimarik <4dimariK@gmail.com>
2020-06-12 11:55:02 +0300

    add isTextMessage

--- src/Request.php
+++ src/Request.php
@@ -24,4 +24,13 @@ class Request
     {
         return !is_null($Update->getMessage());
     }
+    public static function isTextMessage(Update $Update): bool
+    {
+        if (self::isMessage($Update)){
+            return !is_null($Update->getMessage()->getText());
+        } else {
+            return false;
+        }
+
+    }
 }
